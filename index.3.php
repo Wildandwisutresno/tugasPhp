714c8ceb6a7670960a6a893df064ba7cf5e0dbde
Wildandwisutresno <lahasu021@gmail.com>
2025-04-24 11:07:59 +0700

    Tugas1

--- index.3.php
+++ index.3.php
@@ -1,44 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Latihan 2</title>
+    <title>Latihan 1</title>
 </head>
 <body>
-<h2>Daftar Nilai Mahasiswa</h2>
-    <table border="1">
-        <tr>
-            <th>NO</th>
-            <th>NPM</th>
-            <th>NAMA MAHASISWA</th>
-            <th>NILAI</th>
-            <th>KETERANGAN</th>
-        </tr>
-
+    <h2>Angka Kelipatan 3 dan 5 dari 1 sampai 100</h2>
+    <ul>
         <?php
-        $Mahasiswa = [
-            ["npm" => "001", "nama" => "Wildandwi", "nilai" => 80],
-            ["npm" => "002", "nama" => "arasyaqila", "nilai" => 75],
-            ["npm" => "003", "nama " => "Ellumino", "nilai" => 50],
-            ["npm" => "004", "nama" => "elsam", "nilai" => 90],
-            ["npm" => "005", "nama" => "Michael", "nilai" => 50]
-        ];
-
-        $no = 1;
-        foreach ($Mahasiswa as $mhs) {
-            $keterangan = $mhs["nilai"] >= 60 ? "Lulus" : "Tidak Lulus";
-            echo "<tr>";
-            echo "<td>$no</td>";
-            echo "<td>{$mhs['npm']}</td>";
-            echo "<td>{$mhs['nama']}</td>";
-            echo "<td>{$mhs['nilai']}</td>";
-            echo "<td>$keterangan</td>";
-            echo "</tr>";
-            $no++;  
+        for ($i = 1; $i <= 100; $i++) {
+            if ($i % 3 == 0 && $i % 5 == 0) {
+                echo "<li>$i</li>";
+            }
         }
         ?>
-    </table>
-    
+    </ul>
 </body>
 </html>
